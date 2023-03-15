a87e4ca19e0f3a93d9585203a18610d8b3720869
zhengtongle <czxl@163.com>
2023-03-15 23:43:55 +0800

    update .gitignore file

--- service-oa/src/main/java/org/example/auth/service/SysRoleService.java
+++
@@ -1,8 +0,0 @@
-package org.example.auth.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import org.example.model.system.SysRole;
-
-public interface SysRoleService extends IService<SysRole> {
-
-}
