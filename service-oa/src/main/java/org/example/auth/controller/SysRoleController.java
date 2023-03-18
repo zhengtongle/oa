3866167766352ba3acd184cf09917c3be9e90b52
zhengtongle <czxl@163.com>
2023-03-19 02:13:10 +0800

    complete role module

--- service-oa/src/main/java/org/example/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/org/example/auth/controller/SysRoleController.java
@@ -9,12 +9,8 @@ import org.example.model.system.SysRole;
 import org.example.result.Result;
 import org.example.vo.system.SysRoleQueryVo;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @Api(tags = "角色管理接口")
@@ -22,20 +18,20 @@ import java.util.List;
 @RequestMapping("/admin/system/role")
 public class SysRoleController {
 
-    private SysRoleService sysRoleService;
+    private final SysRoleService sysRoleService;
 
     public SysRoleController(SysRoleService sysRoleService) {
         this.sysRoleService = sysRoleService;
     }
 
-    @ApiOperation("查询所有用户")
+    @ApiOperation("查询所有角色")
     @GetMapping("/findAll")
     public Result<List<SysRole>> findAll() {
         return Result.ok(sysRoleService.list());
     }
 
     @ApiOperation("条件分页查询")
-    @GetMapping("{page}/{limit}")
+    @GetMapping("/{page}/{limit}")
     public Result<Page<SysRole>> page(@PathVariable("page") Long page, @PathVariable("limit") Long limit, SysRoleQueryVo sysRoleQueryVo) {
         // 创建Page对象 传递分页参数
         Page<SysRole> pageParam = new Page<>(page, limit);
@@ -48,4 +44,58 @@ public class SysRoleController {
         // 调用实现
         return Result.ok(sysRoleService.page(pageParam, queryWrapper));
     }
+
+    // 添加角色
+    @ApiOperation("添加角色")
+    @PostMapping()
+    public Result<Object> save(@RequestBody SysRole sysRole) {
+        boolean is_success = sysRoleService.save(sysRole);
+        if (is_success) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    // 根据Id查询角色
+    @ApiOperation("根据Id查询角色")
+    @GetMapping("/{id}")
+    public Result<SysRole> get(@PathVariable Long id) {
+        SysRole sysRole = sysRoleService.getById(id);
+        return Result.ok(sysRole);
+    }
+
+    @ApiOperation("根据Id更新角色")
+    @PutMapping()
+    public Result<Object> update(@RequestBody SysRole sysRole) {
+        boolean is_success = sysRoleService.updateById(sysRole);
+        if (is_success) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation("根据Id删除")
+    @DeleteMapping("/{id}")
+    public Result<Object> delete(@PathVariable Long id) {
+        boolean is_success = sysRoleService.removeById(id);
+        if (is_success) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation("批量删除")
+    @DeleteMapping()
+    public Result<Object> delete(@RequestBody List<Long> ids) {
+        boolean is_success = sysRoleService.removeByIds(ids);
+        if (is_success) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
 }
